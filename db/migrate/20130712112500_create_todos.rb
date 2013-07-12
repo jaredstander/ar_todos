3636d1b087549e9058a8424a5a633a79dd0a05d0
dbcapprentice <dbcapprentice@gmail.com>
2013-07-12 12:22:07 -0700

    Updated migrations.

--- db/migrate/20130712112500_create_todos.rb
+++ db/migrate/20130712112500_create_todos.rb
@@ -1,10 +1,8 @@
-require_relative '../config'
-
 class CreateTodos < ActiveRecord::Migration
   def change
     create_table :todos do |t|
       t.integer   :list_id
-      t.task      :task
+      t.string    :task
       t.integer   :status
       t.timestamp :completed_at
       t.timestamps
--- db/migrate/20130712113000_create_lists.rb
+++ db/migrate/20130712113000_create_lists.rb
@@ -1,5 +1,3 @@
-require_relative '../config'
-
 class CreateLists < ActiveRecord::Migration
   def change
     create_table :lists do |l|
