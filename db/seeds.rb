a8c08edaa2318d02d8d5f4cf3c73d026c43ae876
dbc-apprentice <apprentice@devbootcamp.com>
2013-07-12 15:00:38 -0700

    seeded database :)

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,11 @@
-class SeedDB
+require 'faker'
+
+module SeedDB
   def self.seed
     list_names =  ["Chores", "Yardwork", "Grocery List", "Study", "Daily Workout", "Wal-Mart Shopping", "Black-Friday Shopping", "Amazon Shopping"]
     list_names.each do |list_name|
       list = List.create(list_name: list_name)
-
-      list.todos.create(task: "foo")
-        Todo.new(task: "foo", list_id: list.id)
-
-        1.upto(rand(3..10)) {Todo.create(task: Faker::Lorem.sentence(word_count = rand(3..8)))}
+      1.upto(rand(3..10)) {list.todos.create(task: Faker::Lorem.sentence(word_count = rand(3..8)))}
     end
   end
 end
-  
