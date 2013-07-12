d7d499db56fec8611d3260ef6ecbdd64640530fe
dbc-apprentice <apprentice@devbootcamp.com>
2013-07-12 12:16:43 -0700

    created seed file in seeds.rb

---
+++ db/seeds.rb
@@ -0,0 +1,10 @@
+class SeedDB
+  def self.seed
+    list_names =  ["Chores", "Yardwork", "Grocery List", "Study", "Daily Workout", "Wal-Mart Shopping", "Black-Friday Shopping", "Amazon Shopping"]
+    list_names.each do |list_name|
+      List.create(:name => list_name)
+        1.upto(rand(3..10)) {Todo.create(:task => Faker::Lorem.sentence(word_count = rand(3..8)))}
+    end
+  end
+end
+  
