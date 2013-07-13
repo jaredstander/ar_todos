388dedd5677c212fbd84bad09eba177b23e082c0
dbcapprentice <dbcapprentice@gmail.com>
2013-07-12 17:10:44 -0700

    Todo Controller and List Controller. Woot.

--- db/migrate/20130712112500_create_todos.rb
+++ db/migrate/20130712112500_create_todos.rb
@@ -2,7 +2,7 @@ class CreateTodos < ActiveRecord::Migration
   def change
     create_table :todos do |t|
       t.belongs_to  :list
-      t.string    :task
+      t.string      :task
       t.integer   :status, default: 0
       t.timestamp :completed_at, default: nil
       t.timestamps
